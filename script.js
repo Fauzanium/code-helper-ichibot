c8b28b3e58e319d04bddc227ee54371cc9caa07b
DevSPCBot <96945769+DevSPCBot@users.noreply.github.com>
2023-08-22 17:12:55 +0700

    Update

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let index = document.getElementById("index")
 let sensor = document.getElementById("sensor")
 let action = document.getElementById("action")
 let delay = document.getElementById("delay")
-let line_color = document.getElementsByClassName(".line_color")
+let line_color = document.querySelectorAll(".line_color")
 let speed_a = document.getElementById("speed_a")
 let timer = document.getElementById("timer")
 let output = document.getElementById("output")
 sendButton.addEventListener("click", () => {
 
-    console.log(
-        index.value
-    )
+            
+            // console.log(line_color.filter(a => a.checked))
+            console.log(line_color)
         output.innerHTML += `ichibot.setIndex(${index.value}, ${sensor.value}, ${action.value} , ${delay.value} , GARIS_HITAM, ${speed_a.value}, ${timer.value}, PID_3,  KIPAS_OFF, NORMAL); \n`
-        int.value = parseInt(int.value) + 1
+        index.value = parseInt(index.value) + 1
     })
 
 
